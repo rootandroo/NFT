e09cedcb97c4add3edbaa9a2712776d9d637cbb6
andrew <tasks.androo@gmail.com>
2021-10-13 18:37:15 -0400

    Init Vue with Quasar, Vuex, Vue 3

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    // Should be STATIC_URL + path/to/build
+    publicPath: '/static/src/dist/',
+
+    // Output to a directory in STATICFILES_DIRS
+    outputDir: path.resolve(__dirname, '../static/src/dist/'),
+
+    // Django will hash file names, not webpack
+    filenameHashing: false,
+
+    // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+    runtimeCompiler: true,
+
+    devServer: {
+        writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
+    },
+
+    pluginOptions: {
+      quasar: {
+        importStrategy: 'kebab',
+        rtlSupport: false
+      }
+    },
+
+    transpileDependencies: [
+      'quasar'
+    ]
+};
